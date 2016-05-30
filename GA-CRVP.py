17027e18828404b31504dc16e42345bd1a738667
gleydson <gleydson404@gmail.com>
2016-05-30 12:24:16 -0300

    First commit, just with some descriptions about the representation

---
+++ GA-CRVP.py
@@ -0,0 +1,29 @@
+# Representação do invididuo deve serguir a forma:
+# [[costumer#1, costumer#2, costumer#n, fitness, vehicle], 
+# [costumer#1, costumer#2, costumer#n, fitness, vehicle], 
+# [costumer#1, costumer#2, costumer#n, fitness, vehicle]] 
+#
+# onde, 
+#
+# * A quantidade de linhas dessa matriz, sera a quantidade
+# de veiculos
+# * Os campos veiculos e fitness serao fixos
+# * Somente os clientes sofrerao os operadores
+# * O fitness de cada cromossomo(Consideraremos cromossomo
+# como sendo cada linha da matriz) sera atualizado a cada
+# alteracao que o cromossomo sofra
+# * O fitness do invididuo sera a soma do fitness de cada
+# cromossomo
+
+# Os parametros para a execucao geral do algoritmo
+# devem vir de um arquivo parameters.json que deve
+# estar na mesma pasta que este script 
+
+
+import numpy as np
+
+# Acao: Gera um indiviuo
+# parametros: 
+def generate_individual(qtd_vehicles, qtd_costumers):
+    pass    
+
