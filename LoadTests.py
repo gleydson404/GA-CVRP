58a009d48a9ea77d1c2ff03a1582a4438e2ee831
AlexRogaleski <rogaleski@gmail.com>
2016-06-04 17:17:50 -0300

    Troca do tipo de objeto da variavel de retorno clients de dictionay para matriz.

--- LoadTests.py
+++ LoadTests.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 def load(file):
-    clients = {}
+    qtd = int(''.join(([x for x in file if x.isdigit()])[0: 2]))
+    clients = np.zeros((qtd, 4), np.uint8)
+    clients_temp = []
     qtd_clients = 0
     qtd_trucks = 0
     capacity = 0
@@ -11,7 +13,8 @@ def load(file):
     f = open(str(file), 'r')
     for i in f:
         if 'COMMENT' in i:
-            qtd_trucks = int(''.join(([x for x in i[i.find(',') + 1: len(i)][0: i.find(',')] if x.isdigit()])))
+            line = i[i.find(',') + 1: len(i)][0: i.find(',')]
+            qtd_trucks = int(''.join(([x for x in line if x.isdigit()])))
         if 'DIMENSION' in i:
             qtd_clients = int(''.join(([x for x in i if x.isdigit()])))
         if 'CAPACITY' in i:
@@ -21,22 +24,22 @@ def load(file):
             cord = False
         if demand:
             demands = i.split()
-            points = np.array(clients[int(demands[0])])
+            points = np.array(clients_temp[int(demands[0]) - 1])
             demand_point = np.array((int(demands[1])))
             points = np.hstack((points, demand_point))
-            clients[int(demands[0])] = points
+            clients[int(demands[0]) - 1, :] = points
         if 'DEMAND_SECTION' in i:
             demand = True
             cord = False
         if cord:
             line = i.split()
-            clients[int(line[0])] = (int(line[1]), int(line[2]))
+            clients_temp.append([int(line[0]), int(line[1]), int(line[2])])
         if 'NODE_COORD_SECTION ' in i:
             cord = True
     f.close()
-    clients[0] = np.array([0, 0, 0])
     return clients, int(qtd_clients), int(qtd_trucks), int(capacity)
 
-# print load('tests/A-n10-k5.vrp')
-
+# costumer, x, y, z = load('tests/A-n10-k5.vrp')
+# print costumer
+# print costumer[:, 3]
 
